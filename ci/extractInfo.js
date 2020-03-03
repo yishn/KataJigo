8c0af4a0a2b1380aa58dff3442d4e3098e92657a
Yichuan Shen <shen.yichuan@gmail.com>
2020-03-03 23:01:04 +0100

    Add workflow for release creation

---
+++ ci/extractInfo.js
@@ -0,0 +1,13 @@
+const {version} = require('../package.json')
+
+function printSetOutputs(outputs) {
+  for (let [name, value] of Object.entries(outputs)) {
+    console.log(`::set-output name=${name}::${value}`)
+  }
+}
+
+printSetOutputs({
+  version,
+  tag: (process.env.GITHUB_REF || '').replace('refs/tags/', ''),
+  ci: path.resolve(process.cwd(), './ci')
+})
