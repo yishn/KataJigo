0dd68db8c0e58b7797feb8af7b7283db6b018140
Yichuan Shen <shen.yichuan@gmail.com>
2020-03-04 00:23:38 +0100

    Pipe stdin to KataGo if not in GTP mode

--- src/main.js
+++ src/main.js
@@ -59,6 +59,10 @@ async function main() {
       controller.process.stdout.on('data', chunk => {
         process.stdout.write(chunk)
       })
+
+      process.stdin.on('data', chunk => {
+        controller.process.write(chunk)
+      })
     }
   })
 
